61090c7dfdfd2e24894989f9e5b0951a18eda305
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-06-29 12:59:07 +0530

    first todo

---
+++ type-annotations/deepDiveInTypeAnnotations.js
@@ -0,0 +1,4 @@
+//todo 1 substring
+var longText = "abhishekwas out here, coding.";
+var shortText = longText.substring(0, 10);
+console.log("shortText=", shortText);
---
+++ type-annotations/deepDiveInTypeAnnotations.ts
@@ -0,0 +1,7 @@
+//todo 1 substring
+
+let longText:string = "abhishekwas out here, coding."
+
+let shortText:string =longText.substring(0,10)
+
+console.log("shortText=",shortText)
