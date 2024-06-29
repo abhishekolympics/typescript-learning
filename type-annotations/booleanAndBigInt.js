267144fd00b18e0ff64cf9a382990be53c8244d1
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-06-29 13:18:29 +0530

    create function for checking divisibility by 4 and 8

--- type-annotations/booleanAndBigInt.js
+++ type-annotations/booleanAndBigInt.js
@@ -4,3 +4,9 @@ var isEven = function (val) {
 };
 console.log(isEven(444));
 console.log(isEven(463453));
+//check for divisibility by 4 and 8
+var isDivisibleBy4And8 = function (value) {
+    return (value % 4 === 0 && value % 8 === 0);
+};
+console.log(isDivisibleBy4And8(48));
+console.log(isDivisibleBy4And8(52));
--- type-annotations/booleanAndBigInt.ts
+++ type-annotations/booleanAndBigInt.ts
@@ -6,3 +6,12 @@ const isEven = (val:number):boolean => {
 
 console.log(isEven(444));
 console.log(isEven(463453));
+
+//check for divisibility by 4 and 8
+
+const isDivisibleBy4And8 = (value:number):boolean => {
+    return (value%4 === 0 && value%8 === 0);
+}
+
+console.log(isDivisibleBy4And8(48))
+console.log(isDivisibleBy4And8(52))
