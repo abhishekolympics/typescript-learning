9e5d8d524f4a0858e85df1fad1a5d1a9a0273782
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-06-29 13:14:47 +0530

    create function for checking even/odd

---
+++ type-annotations/booleanAndBigInt.js
@@ -0,0 +1,6 @@
+//check even number
+var isEven = function (val) {
+    return (val % 2 === 0);
+};
+console.log(isEven(444));
+console.log(isEven(463453));
---
+++ type-annotations/booleanAndBigInt.ts
@@ -0,0 +1,8 @@
+//check even number
+
+const isEven = (val:number):boolean => {
+    return (val%2 === 0);
+}
+
+console.log(isEven(444));
+console.log(isEven(463453));
